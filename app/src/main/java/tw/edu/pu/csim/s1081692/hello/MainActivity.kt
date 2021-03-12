6cdaa8e55c72544e687d62a935eb3e4b168610f4
Yomin <s1081692@gm.pu.edu.tw>
2021-03-12 15:21:19 +0800

    第二次Commit

--- app/src/main/java/tw/edu/pu/csim/s1081692/hello/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081692/hello/MainActivity.kt
@@ -2,10 +2,18 @@ package tw.edu.pu.csim.s1081692.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+
+
+
     }
 }
